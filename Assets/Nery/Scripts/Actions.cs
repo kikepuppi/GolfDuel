70f95ba0dbd8acac8ea25b15352c195af5fbc40b
Pedro Nery <pedronas1201@gmail.com>
2026-05-08 18:42:31 -0300

    Menu melhorado e painel de créditos iniciado

--- Assets/Nery/Scripts/Actions.cs
+++ Assets/Nery/Scripts/Actions.cs
@@ -3,6 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour
 {
+
+    void Start() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null) {
+            float worldHeight = Camera.main.orthographicSize * 2f;
+            float worldWidth = worldHeight * Camera.main.aspect;
+            transform.localScale = new Vector3(
+                worldWidth / sr.sprite.bounds.size.x,
+                worldHeight / sr.sprite.bounds.size.y,
+                1f
+            );
+        }
+    }
+    
     public void IniciaConfig()
     {
        SceneManager.LoadScene(1);
@@ -12,4 +26,24 @@ public class MenuActions : MonoBehaviour
     {
        SceneManager.LoadScene(2);
     }
+    
+    public GameObject creditsPanel;
+    public GameObject instructionsPanel;
+
+    public void AbreCredits() {
+        creditsPanel.SetActive(true);
+    }
+
+    public void FechaCredits() {
+        creditsPanel.SetActive(false);
+    }
+
+    public void AbreInstructions() {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void FechaInstructions() {
+        instructionsPanel.SetActive(false);
+    }
+    
 }
