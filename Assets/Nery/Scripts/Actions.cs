c5f40e8764905ed7ed2887788211da62b549c110
Pedro Nery <pedronas1201@gmail.com>
2026-05-11 11:45:19 -0300

    Painel de créditos feito + instructions (faltando apenas (de)buffs)

--- Assets/Nery/Scripts/Actions.cs
+++ Assets/Nery/Scripts/Actions.cs
@@ -3,47 +3,49 @@ using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour
 {
+    public SpriteRenderer background;
+
+    public GameObject menuPanel;
+    public GameObject creditsPanel;
+    public GameObject instructionsPanel;
 
     void Start() {
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (sr != null) {
+        if (background != null) {
             float worldHeight = Camera.main.orthographicSize * 2f;
             float worldWidth = worldHeight * Camera.main.aspect;
-            transform.localScale = new Vector3(
-                worldWidth / sr.sprite.bounds.size.x,
-                worldHeight / sr.sprite.bounds.size.y,
+            background.transform.localScale = new Vector3(
+                worldWidth / background.sprite.bounds.size.x,
+                worldHeight / background.sprite.bounds.size.y,
                 1f
             );
         }
     }
-    
-    public void IniciaConfig()
-    {
-       SceneManager.LoadScene(1);
+
+    public void IniciaConfig() {
+        SceneManager.LoadScene(1);
     }
-    
-    public void IniciaJogo()
-    {
-       SceneManager.LoadScene(2);
+
+    public void IniciaJogo() {
+        SceneManager.LoadScene(2);
     }
-    
-    public GameObject creditsPanel;
-    public GameObject instructionsPanel;
 
     public void AbreCredits() {
+        menuPanel.SetActive(false);
         creditsPanel.SetActive(true);
     }
 
     public void FechaCredits() {
         creditsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 
     public void AbreInstructions() {
+        menuPanel.SetActive(false);
         instructionsPanel.SetActive(true);
     }
 
     public void FechaInstructions() {
         instructionsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
-    
 }
